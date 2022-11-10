01c002dba4f57c3942269a0fd592aed038529717
Artur <artur.dudnik@inbox.ru>
2022-11-10 13:48:33 +0200

    update path, change work with API

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const HOST_CRAFT_COORDINATE = 'http://api.open-notify.org/iss-now.json'; // Take data coordinate ISS
-export const HOST_SPACEMAN_NAMES = 'http://api.open-notify.org/astros.json'; // Take data spacemen list
+export const HOST_CRAFT_COORDINATE = 'https://api.open-notify.org/iss-now.json'; // Take data coordinate ISS
+export const HOST_SPACEMAN_NAMES = 'https://api.open-notify.org/astros.json'; // Take data spacemen list
 export const TOKEN_GOOGLE_API = process.env.TOKEN; // Api google for map
